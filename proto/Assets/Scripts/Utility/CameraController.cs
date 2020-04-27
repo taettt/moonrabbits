7f1c251e7d76ee447a08d445373d768a4240fd7c
taettt <39712594+taettt@users.noreply.github.com>
2020-04-27 09:18:15 +0900

    카메라 관련
    카메라 스크립트 수정본
    uiCam 코드 부분은 주석처리 계속 해놓으시고 사용하시면 될 것 같습니다

--- proto/Assets/Scripts/Utility/CameraController.cs
+++ proto/Assets/Scripts/Utility/CameraController.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Vector3 pos = new Vector3(-11.0f, 18.0f, -11.0f);
-    public float offset;
     public float speed;
     public float zoomDis;
+    public float zoomInFov;
+    public float zoomOutFov;
 
     public Transform playerTr;
     public Transform enemyTr;
     private Transform tr;
     private Camera thisCamera;
+    public Transform uiCam;
 
     void Awake()
     {
@@ -22,40 +24,15 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        //tr.position = new Vector3(playerTr.position.x, playerTr.position.y + offset, playerTr.position.z);
-        this.transform.position = playerTr.position + pos;
+        tr.position = Vector3.Lerp(playerTr.position, enemyTr.position, 0.4f) + pos;
+        //uiCam.position = playerTr.position + pos;
 
-        //DistanceDirect();
+        DistanceDirect();
     }
 
     private void DistanceDirect()
     {
-        float distance = Vector3.Distance(playerTr.position, enemyTr.position);
-        if(distance<zoomDis)
-        {
-            ControlSize(7.0f);
-        }
-        else
-        {
-            ControlSize(12.0f);
-        }
-    }
-
-    public void ControlSize(float size)
-    {
-        thisCamera.orthographicSize = Mathf.Lerp(thisCamera.orthographicSize,
-            size, Time.deltaTime * speed);
-    }
-
-    public void ControlAngleX(float angle)
-    {
-        Vector3 curRot = new Vector3(tr.rotation.x, tr.rotation.y, tr.rotation.z);
-        tr.eulerAngles =Vector3.Lerp(curRot, new Vector3(angle, curRot.y, curRot.z), Time.deltaTime * speed);
-    }
-
-    public void ControlAngleY(float angle)
-    {
-        Vector3 curRot = new Vector3(tr.rotation.x, tr.rotation.y, tr.rotation.z);
-        tr.eulerAngles = Vector3.Lerp(curRot, new Vector3(curRot.x, angle, curRot.z), Time.deltaTime * speed);
+        float dis = Vector3.Distance(playerTr.position, enemyTr.position);
+        thisCamera.fieldOfView = Mathf.Lerp(zoomInFov, zoomOutFov, dis / 20.0f);
     }
 }
